0ebd80fe67b7d066bce3a3749922131b8bef47ec
Paulo Toledo <paulodat.902522195@uniacademia.edu.br>
2021-05-29 18:10:54 -0300

    Iniciando o projeto

---
+++ ativcompl/src/main/java/br/edu/uniacademia/ativcompl/AtivcomplApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uniacademia.ativcompl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtivcomplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtivcomplApplication.class, args);
+	}
+
+}
---
+++ ativcompl/src/test/java/br/edu/uniacademia/ativcompl/AtivcomplApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.uniacademia.ativcompl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtivcomplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
