30d088e94692a6bc13482c4bea029b2cea628f0d
Paulo Toledo <paulodat.902522195@uniacademia.edu.br>
2021-05-29 19:17:25 -0300

    Banco de dados H2 e ciracao automatica da base de dados

--- ativcompl/src/main/java/br/edu/uniacademia/ativcompl/domain/Address.java
+++ ativcompl/src/main/java/br/edu/uniacademia/ativcompl/domain/Address.java
@@ -2,9 +2,17 @@ package br.edu.uniacademia.ativcompl.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Address implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String street;
 	private String number;
