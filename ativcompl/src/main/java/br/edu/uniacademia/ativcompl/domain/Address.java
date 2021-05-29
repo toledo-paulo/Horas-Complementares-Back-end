48b8e4e6bf223e41d1b31d851a07077e65b546c2
Paulo Toledo <paulodat.902522195@uniacademia.edu.br>
2021-05-29 18:51:47 -0300

    Primeira classe domain - Address

---
+++ ativcompl/src/main/java/br/edu/uniacademia/ativcompl/domain/Address.java
@@ -0,0 +1,91 @@
+package br.edu.uniacademia.ativcompl.domain;
+
+import java.io.Serializable;
+
+public class Address implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String street;
+	private String number;
+	private String district;
+	private String city;
+	
+	public Address() {}
+
+	public Address(Long id, String street, String number, String district, String city) {
+		super();
+		this.id = id;
+		this.street = street;
+		this.number = number;
+		this.district = district;
+		this.city = city;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+
+	public String getDistrict() {
+		return district;
+	}
+
+	public void setDistrict(String district) {
+		this.district = district;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+}
--- ativcompl/src/main/java/br/edu/uniacademia/ativcompl/resources/AddressResource.java
+++ ativcompl/src/main/java/br/edu/uniacademia/ativcompl/resources/AddressResource.java
@@ -1,15 +1,27 @@
 package br.edu.uniacademia.ativcompl.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.uniacademia.ativcompl.domain.Address;
 
 @RestController
 @RequestMapping(value="/address")
 public class AddressResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Address> listar() {
+		
+		Address ad1 = new Address((long) 1, "Ruas das Angélicas", "20", "N. Sra. das Graças", "Lima Duarte");
+		Address ad2 = new Address((long)2, "Ruas Leonides M. Campos", "74", "Centro", "Lima Duarte");
+		
+		List<Address> lista = new ArrayList<>();
+		lista.add(ad1);
+		lista.add(ad2);
+		return lista;
 	}
 }
